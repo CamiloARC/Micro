649e0cc95fb6506427a36157d127dbfdcd9fea0e
CamiloARC <camilo._car@hotmail.com>
2015-08-13 12:51:51 -0500

    se modifico main.c

--- main.c
+++ main.c
@@ -3,5 +3,28 @@
 
 int main(void)
 {
-return 0;
+	int operacion,a,b,x;
+	printf("Ingrese primer entero: ");
+	scanf("%d",&a);
+	printf("Ingrese segundo entero: ");
+	scanf("%d",&b);
+	printf("¿Que operacion desea realizar?\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division");
+	scanf("%d",&operacion);
+	switch (operacion)
+	{
+		case 1: 	
+		x=suma(a,b);
+		break;
+		case 2:
+		x=resta(a,b);
+		break;
+		case 3:
+		x=multiplicacion(a,b);
+		break;
+		case 4:
+		x=division(a,b);
+		break;
+	};
+	printf("El resutado es: %d",x);
+	return 0;
 }
